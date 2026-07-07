ec0bf4a11658003be910c3896122a7e098bfc255
decaquestionmarks <nateyheit@gmail.com>
2026-07-07 10:50:57 -0700

    PartyPokemon Methods and Skeletons

--- Pokemon/PartyPokemon.hpp
+++ Pokemon/PartyPokemon.hpp
@@ -28,12 +28,14 @@ class PartyPokemon{
     //int obediencelevel; not sure what this does, so leaving out
     bool metAsEgg;
     //Stats
+    int currHP;
     int hpIV;
     int atkIV;
     int defIV;
     int spaIV;
     int spdIV;
     int speIV;
+    bool bottlecapped[6];
     int hpEV;
     int atkEV;
     int defEV;
@@ -41,7 +43,10 @@ class PartyPokemon{
     int spdEV;
     int speEV;
     //Moves
-    
+    std::string Moves[4];
+    int MovePP[4];
+    int MovePPUps[4];
+    //std::vector<std::string> pastmoves;
     //Cosmetic Data
         //Nah
     //OT
@@ -58,4 +63,66 @@ class PartyPokemon{
     int addExp(int expGain);
     bool willLevelUp() const;
     bool doLevelUp();
+    std::string getNature() const {return nature;}
+    std::string getEffectiveNature() const {return effectiveNature;}
+    void setEffectiveNature(std::string newNature){effectiveNature = newNature;};
+    std::string getAbility() const {return ability;}
+    void setAbility(std::string newAbility){ability = newAbility;};
+    std::string getHeldItem() const {return heldItem;}
+    void setHeldItem(std::string newItem){heldItem = newItem;};
+    int getFriendship() const {return friendship;}
+    void addFriendship(int amount){friendship += amount;}
+    bool isEgg() const {return isEgg;}
+    void hatch(); // Stuff to generate when an egg is hatched
+    bool isShiny() const {return isShiny;}
+    bool isPKRSInfected() const {return pkrsinfected;}
+    void infectPKRS() {pkrsinfected = true;}
+    void curePKRS() {pkrscured = pkrsinfected;} // can't cure without being infected
+    bool isPKRSCured() const {return pkrscured;}
+    int getPID() const {return PID;}
+
+    std::string getMetLocation() const {return metLocation;}
+    std::string getCaughtBall() const {return caughtBall;}
+    void setCaughtBall(std::string newBall){caughtBall = newBall;}
+    int getMetLevel() const {return metLevel;}
+    bool isMetAsEgg() const {return metAsEgg;}
+
+    int getCurrHP() const {return currHP;}
+    void setCurrHP(int newHP) {currHP = newHP;}
+    void addHP(int amount) {currHP += amount;}
+    void subtractHP(int amount) {currHP -= amount;}
+    int getHPIV() const {return hpIV;}
+    int getAtkIV() const {return atkIV;}
+    int getDefIV() const {return defIV;}
+    int getSpaIV() const {return spaIV;}
+    int getSpdIV() const {return spdIV;}
+    int getSpeIV() const {return speIV;}
+    bool isBottleCapped(int index) const {return bottlecapped[index];}
+    void setBottleCapped(int index, bool capped) {bottlecapped[index] = capped;}
+    int getHPEV() const {return hpEV;}
+    int getAtkEV() const {return atkEV;}
+    int getDefEV() const {return defEV;}
+    int getSpaEV() const {return spaEV;}
+    int getSpdEV() const {return spdEV;}
+    int getSpeEV() const {return speEV;}
+    void addHPEV(int amount) {hpEV += amount;}
+    void addAtkEV(int amount) {atkEV += amount;}
+    void addDefEV(int amount) {defEV += amount;}
+    void addSpaEV(int amount) {spaEV += amount;}
+    void addSpdEV(int amount) {spdEV += amount;}
+    void addSpeEV(int amount) {speEV += amount;}
+
+    std::string getMove(int index) const {return Moves[index];}
+    void setMove(int index, std::string newMove) {Moves[index] = newMove;}
+    int getMovePP(int index) const {return MovePP[index];}
+    void setMovePP(int index, int newPP) {MovePP[index] = newPP;}
+    int getMovePPUp(int index) const {return MovePPUps[index];}
+    void setMovePPUp(int index, int newPPUp) {MovePPUps[index] = newPPUp;}
+    void learnMove(int index, std::string newMove); //do full process
+    void swapMoves(int index1, int index2); //do full process
+    void forgetMove(int index); //remove move then move all moves up
+
+    std::string getOTName() const {return OTName;}
+    int getOTID() const {return OTID;}
+    int getSID() const {return SID;}
 };
